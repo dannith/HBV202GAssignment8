2dde00baf9876d7798ac2aeba2b6690752389d46
dannith <dannitorr@gmail.com>
2023-03-07 10:36:51 +0000

    add User, Student & FacultyMember

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student extends User{
+
+
+    private boolean feePaid;
+    protected Student(String name, boolean feePaid) {
+        super(name);
+        this.feePaid = feePaid;
+    }
+
+    public boolean isFeePaid() {
+        return feePaid;
+    }
+
+    public void setFeePaid(boolean feePaid) {
+        this.feePaid = feePaid;
+    }
+}
